c47580585111099051f6b043970feb1a7b42b4b3
Hector Vasquez <hvasquezdev@gmail.com>
2020-06-24 19:55:39 -0400

    Update status

--- components/home/Status.vue
+++ components/home/Status.vue
@@ -6,10 +6,10 @@
       class="work-status__wrapper flex flex-col justify-center items-center ml-auto mr-auto w-full text-center lg:flex-row lg:justify-between"
     >
       <h3 class="font-nunito font-bold text-dark-primary">
-        I am available to talk about new contracts
+        Do you want to talk with me?
       </h3>
       <base-button v-scroll-to="'#contact'" animated color="yellow">
-        Hire Me
+        Send Message
       </base-button>
     </div>
   </section>
