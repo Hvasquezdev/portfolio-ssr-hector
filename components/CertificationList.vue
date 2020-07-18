99d36c8db4f8744c64eaff348345aedd08c63d58
Hector Vasquez <hvasquezdev@gmail.com>
2020-07-17 23:56:40 -0400

    Add certification section

---
+++ components/CertificationList.vue
@@ -0,0 +1,148 @@
+<template>
+  <div v-show="isLoaded" ref="masonry" class="masonry">
+    <template v-if="columsElements.length > 0 && isLoaded">
+      <div
+        v-for="(column, index) in columsElements"
+        :key="index"
+        class="masonry__column"
+      >
+        <template v-for="(item, itemIndex) in column.items">
+          <project-card
+            :key="item.name + itemIndex"
+            :image-url="getImage(item.image)"
+            :alt="item.name"
+            :image-placeholder="getPlaceholderImage()"
+            :link="item.link || ''"
+          ></project-card>
+        </template>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    ProjectCard: () => import('@/components/ProjectCard.vue')
+  },
+  data() {
+    return {
+      items: [
+        {
+          image: 'arquitecto-frontend.png',
+          name: 'Platzi - Arquitecto FrontEnd'
+        },
+        {
+          image: 'carrera-vue.png',
+          name: 'Platzi - Desarrollo con Vue.Js'
+        },
+        {
+          image: 'carrera-js.png',
+          name: 'Platzi - Escuela de JavaScript'
+        },
+        {
+          image: 'js-profesional.png',
+          name: 'Platzi - Javascript Profesional'
+        }
+      ],
+      columns: 1,
+      columsElements: [],
+      addedElements: 0,
+      isLoaded: false
+    };
+  },
+  computed: {
+    itemsToRender() {
+      return Math.ceil(this.items.length / this.columns);
+    }
+  },
+  mounted() {
+    this.setColumns();
+
+    window.addEventListener('resize', this.setColumns, false);
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.setColumns, false);
+  },
+  methods: {
+    setColumns() {
+      const screenWidth = window.innerWidth;
+
+      if (screenWidth < 768) {
+        this.columns = 1;
+      } else if (screenWidth >= 768 && screenWidth < 1360) {
+        this.columns = 2;
+      } else {
+        this.columns = 3;
+      }
+
+      this.initMasonry(this.columns);
+    },
+    getImage(name) {
+      return `/images/${name}`;
+    },
+    getPlaceholderImage() {
+      return '/images/placeholder-image-min.jpg';
+    },
+    initMasonry(columns) {
+      this.isLoaded = false;
+      const masonry = this.$refs.masonry;
+
+      masonry.classList.remove('columns-1', 'columns-2', 'columns-3');
+      masonry.classList.add(`columns-${columns}`);
+      this.createColumElements();
+    },
+    createColumElements() {
+      this.addedElements = 0;
+      let columsElements = [];
+
+      for (let i = 0; i < this.columns; i++) {
+        columsElements.push({ items: [] });
+      }
+
+      this.columsElements = columsElements;
+      this.addColumnItems();
+    },
+    addColumnItems() {
+      const itemsToRender = this.itemsToRender;
+      const columns = this.columns;
+
+      for (let i = 0; i < itemsToRender; i++) {
+        for (let n = 0; n < columns; n++) {
+          const items = this.items;
+          const addedElements = this.addedElements;
+
+          if (addedElements < items.length) {
+            const itemToPush = items[i * columns + n];
+            this.columsElements[n].items.push(itemToPush);
+            this.addedElements++;
+          }
+        }
+      }
+
+      this.isLoaded = true;
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+.masonry {
+  --grid-columns: 1;
+  --grid-columns-gap: 30px;
+
+  max-width: 1170px;
+  display: grid;
+  grid-template-columns: repeat(var(--grid-columns), 1fr);
+  column-gap: var(--grid-columns-gap);
+}
+.masonry.columns-2 {
+  --grid-columns: 2;
+}
+.masonry.columns-3 {
+  --grid-columns: 3;
+}
+.masonry__column .masonry__card:not(:last-child) {
+  margin-bottom: 30px;
+}
+</style>
--- components/home/Projects.vue
+++ components/home/Projects.vue
@@ -10,17 +10,41 @@
       Here are some of the works that I made as a Front End developer and in
       some working as a FullStack
     </p>
-    <h3 class="font-poppins font-medium text-green-primary relative">
-      Web Development
-    </h3>
-    <projects-list />
+
+    <div class="tabs">
+      <button
+        :class="{ 'is-active': activeTab === 'projects' }"
+        class="tab-btn font-poppins font-light text-grey-secondary relative"
+        @click="activeTab = 'projects'"
+      >
+        Web Development
+      </button>
+      <button
+        :class="{ 'is-active': activeTab === 'certifications' }"
+        class="tab-btn font-poppins text-grey-secondary font-light relative"
+        @click="activeTab = 'certifications'"
+      >
+        Certifications
+      </button>
+    </div>
+    <keep-alive>
+      <projects-list v-if="activeTab === 'projects'" />
+      <certification-list v-if="activeTab === 'certifications'" />
+    </keep-alive>
   </section>
 </template>
 
 <script>
 export default {
   components: {
-    ProjectsList: () => import('@/components/ProjectsList.vue')
+    ProjectsList: () => import('@/components/ProjectsList.vue'),
+    CertificationList: () => import('@/components/CertificationList.vue')
+  },
+
+  data() {
+    return {
+      activeTab: 'projects'
+    };
   }
 };
 </script>
@@ -39,20 +63,39 @@ export default {
   padding-bottom: 45px;
   max-width: 535px;
 }
-#home .projects h3 {
+#home .projects .tabs {
   margin-bottom: 90px;
+  display: grid;
+  grid-template-columns: repeat(2, auto);
+  row-gap: 20px;
+  column-gap: 30px;
+}
+#home .projects .tabs .tab-btn {
   font-size: 16px;
   line-height: 27px;
   cursor: pointer;
+  outline: none;
+  transition: all 0.3s;
 }
-#home .projects h3::after {
+#home .projects .tabs .tab-btn:active,
+#home .projects .tabs .tab-btn:focus {
+  outline: none;
+}
+#home .projects .tabs .tab-btn.is-active {
+  font-weight: 500;
+  color: theme('colors.green.primary');
+}
+#home .projects .tabs .tab-btn::after {
   content: '';
   background-color: theme('colors.green.primary');
   height: 1px;
-  width: 45%;
+  width: 0px;
   position: absolute;
   bottom: -3.5px;
   left: 0;
   transition: all 0.3s;
+}
+#home .projects .tabs .tab-btn.is-active::after {
+  width: 45%;
 }
 </style>
