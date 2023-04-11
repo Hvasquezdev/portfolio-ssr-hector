af3264051825986bfbffae013f6e11bafb973a2d
Hector Vazquez <hvasquezdev@gmail.com>
2023-04-11 10:38:26 -0300

    feat(TheFooter): add new phone number and update style

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -3,58 +3,68 @@
     class="footer text-center w-full flex flex-col justify-center items-center"
   >
     <div class="footer__info w-full">
-      <div class="footer__info__item">
-        <span class="icon text-white">
-          <client-only>
-            <font-awesome-icon :icon="['fas', 'phone-alt']" />
-          </client-only>
-        </span>
-        <div class="text font-nunito">
-          <strong class="font-nunito">
-            Phone:
-          </strong>
-          Whatsapp: (+58)412-8580282
+      <div class="px-8 flex gap-5 flex-wrap justify-between">
+        <div class="footer__info__item">
+          <span class="icon text-white">
+            <client-only>
+              <font-awesome-icon :icon="['fas', 'phone-alt']" />
+            </client-only>
+          </span>
+          <div class="text font-nunito">
+            <strong class="font-nunito">
+              Phone:
+            </strong>
+            Whatsapp: (+58) 412-8580282
+          </div>
+        </div>
+        <div class="footer__info__item">
+          <span class="icon text-white">
+            <client-only>
+              <font-awesome-icon :icon="['fas', 'phone-alt']" />
+            </client-only>
+          </span>
+          <div class="text font-nunito">
+            <strong class="font-nunito">
+              Phone:
+            </strong>
+            (+54) 011 51029271
+          </div>
+        </div>
+        <div class="footer__info__item">
+          <span class="icon text-white">
+            <client-only>
+              <font-awesome-icon :icon="['fas', 'envelope']" />
+            </client-only>
+          </span>
+          <div class="text font-nunito">
+            <strong class="font-nunito">
+              Email:
+            </strong>
+            hvasquezdev@gmail.com
+          </div>
+        </div>
+        <div class="footer__info__item">
+          <span class="icon text-white">
+            <client-only>
+              <font-awesome-icon :icon="['fas', 'map-marker-alt']" />
+            </client-only>
+          </span>
+          <div class="text font-nunito">
+            <strong class="font-nunito">
+              My Address:
+            </strong>
+            <address>
+              Buenos Aires, Argentina
+            </address>
+          </div>
         </div>
       </div>
-      <div class="footer__info__item">
-        <span class="icon text-white">
-          <client-only>
-            <font-awesome-icon :icon="['fas', 'envelope']" />
-          </client-only>
-        </span>
-        <div class="text font-nunito">
-          <strong class="font-nunito">
-            Email:
-          </strong>
-          hvasquezdev@gmail.com
-        </div>
-      </div>
-      <div class="footer__info__item">
-        <span class="icon text-white">
-          <client-only>
-            <font-awesome-icon :icon="['fas', 'map-marker-alt']" />
-          </client-only>
-        </span>
-        <div class="text font-nunito">
-          <strong class="font-nunito">
-            My Address:
-          </strong>
-          <address>
-            Ciudad Bolívar, Venezuela
-          </address>
-        </div>
-      </div>
-      <div class="footer__info__address"></div>
     </div>
     <p class="font-nunito text-white-primary">
       Made with &#60;3 by Hector Vasquez
     </p>
   </footer>
 </template>
-
-<script>
-export default {};
-</script>
 
 <style lang="css" scoped>
 .footer {
@@ -66,10 +76,6 @@ export default {};
   padding-bottom: 28px;
   border-bottom: 1px solid theme('colors.white.primary');
   margin-bottom: 48px;
-  display: grid;
-  grid-template-columns: repeat(1, minmax(320px, 1fr));
-  column-gap: 20px;
-  row-gap: 20px;
 }
 .footer__info__item {
   display: flex;
